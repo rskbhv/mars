1ee158ea17f03619f73db13313c1cd6740841ec9
BHAMIDIPATIRSK <BHAMIDIPATIRSK@BHAMIDIPATI-RSK>
2020-06-15 14:34:14 +0530

    Initial Commit

---
+++ MARS-Telecom-codingAssignmentTest/src/main/java/com/mars/MarsTelecomCodingAssignmentTestApplication.java
@@ -0,0 +1,13 @@
+package com.mars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsTelecomCodingAssignmentTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsTelecomCodingAssignmentTestApplication.class, args);
+	}
+
+}
